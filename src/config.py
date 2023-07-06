66b649edef7ab195ffc3e32ace9866bf0f7bab14
CookieDecide <julian.lauermann@freenet.de>
2023-07-06 12:11:24 +0200

    change wd to root folder

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ Licensed under MIT License
 
 command_prefix = ">"  # prefix to invoke a command in discord message
 bot_description = "Discord bot"  # Description of the Discord bot
-log_path = "../logs/"  # Path of log file. Currently only one log file to be used
-youtube_output_path = "../YT/"
+log_path = "logs/"  # Path of log file. Currently only one log file to be used
+youtube_output_path = "YT/"
 youtube_file_extension = "mp4"

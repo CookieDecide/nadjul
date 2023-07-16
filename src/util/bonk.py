90e56a93288a30d7406a9e9758252e2625960b7d
CookieDecide <61245108+CookieDecide@users.noreply.github.com>
2023-07-16 12:05:22 +0200

    added bonk command (#26)
    * added bonk command, created img folder
    
    * added bot ready event (#25)
    
    * updated bonk.py description
    
    * added bonk command, created img folder
    
    * updated bonk.py description
    
    * added img path to config

---
+++ src/util/bonk.py
@@ -0,0 +1,33 @@
+"""Copyright by CookieDecide, Darkuuu
+Licensed under MIT License
+Contains helper functions to create bonk images.
+"""
+
+import requests
+from PIL import Image
+import config
+import logging
+
+
+def create_bonk_img(avatar_url) -> Image.Image:
+    """Creates a bonk image withe the given discord avatar.
+
+    Args:
+        avatar_url: Url to the users avatar.
+
+    Returns:
+        Image of the avatar being bonked.
+    """
+    bonk = Image.open(config.img_path + "bonk.png")
+
+    avatar = Image.open(
+        requests.get(
+            "https://cdn.discordapp.com/avatars/132557567585943553/fd58edf119cf9127e5863728da356563.png?size=1024",
+            stream=True,
+        ).raw
+    )
+    avatar = avatar.resize((256, 256))
+
+    bonk.paste(avatar, (700, 400))
+
+    return bonk
